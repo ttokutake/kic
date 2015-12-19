0f87e81da160a1b401ffc51d1729be4ea54bd637
aTadatoshiTokutake <tadatoshi.tokutake@access-company.com>
2015-12-19 20:41:25 +0900

    Create scaffold

---
+++ README.md
@@ -0,0 +1,3 @@
+# Keepha
+
+under construction
---
+++ src/main.rs
@@ -0,0 +1,70 @@
+use std::env;
+
+fn main() {
+    let args = env::args().skip(1).collect::<Vec<String>>();
+
+    if args.is_empty() {
+        print_usage();
+        return
+    }
+
+    match args[0].as_ref() {
+        "help"    => print_usage(),
+        "init"    => initialize(),
+        "start"   => register_with_cron(),
+        "end"     => unregister_cron(),
+        "set"     => set_params(),
+        "clean"   => clean(),
+        "destroy" => destroy(),
+        _         => print_usage(),
+    }
+}
+
+fn print_usage() {
+    let full_path_to_bin = env::current_exe().unwrap();
+    let bin_name         = full_path_to_bin.file_name()
+        .unwrap()
+        .to_str()
+        .unwrap();
+    println!(
+"Usage:
+    {} <command>
+
+Commands:
+    help    # Print this message.
+    init    # Register current directory.
+    start   # Start \"{}\"
+    end     # End \"{}\"
+    set     # Set parameters.
+    clean   # Clean current directory.
+    destroy # Destroy \"{}\"",
+        bin_name,
+        bin_name,
+        bin_name,
+        bin_name,
+    );
+}
+
+fn initialize() {
+    println!("initialize");
+}
+
+fn register_with_cron() {
+    println!("register");
+}
+
+fn unregister_cron() {
+    println!("unregister");
+}
+
+fn set_params() {
+    println!("set parameters");
+}
+
+fn clean() {
+    println!("clean");
+}
+
+fn destroy() {
+    println!("destroy");
+}
