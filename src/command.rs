4034660e0fb083f507e9be0bb5140f3c9fd2ba5d
aTadatoshiTokutake <tadatoshi.tokutake@access-company.com>
2015-12-19 22:15:45 +0900

    Implement "init" command

--- src/command.rs
+++ src/command.rs
@@ -1,4 +1,30 @@
+use std::fs;
+use std::path::Path;
+
 pub fn initialize() {
+    println!("Initialize ...");
+
+    let dir_name = ".keepha";
+    if !dir_exists(dir_name) {
+        match fs::create_dir(dir_name) {
+            Ok(_)    => println!("  OK: Created \"{}\" directory.", dir_name),
+            Err(why) => panic!("{:?}", why),
+        }
+    } else {
+        println!("  OK: \"{}\" directory has already exist.", dir_name);
+    }
+}
+
+fn dir_exists<P: AsRef<Path>>(path: P) -> bool {
+    match fs::metadata(path) {
+        Ok(m)    => m.is_dir(),
+        Err(why) => {
+            match why.raw_os_error() {
+                Some(2) => false,
+                _       => panic!("{:?}", why),
+            }
+        }
+    }
 }
 
 pub fn set_params() {
