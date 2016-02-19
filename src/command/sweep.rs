b795fd39ebfb66c440781b12dc5ace9396ee9390
aTadatoshiTokutake <tadatoshi.tokutake@access-company.com>
2016-02-19 18:33:03 +0900

    Create two functions for readablity

--- src/command/sweep.rs
+++ src/command/sweep.rs
@@ -23,7 +23,7 @@ impl Command for Sweep {
         let now  = Local::now();
         let date = format!("{}", now.format("%Y-%m-%d"));
 
-        let path_to_dust_box = path_buf![storage_dir(), date, "dust"];
+        let path_to_dust_box = path_buf![storage_dir(), date, "dusts"];
         create_essential_dir_all(&path_to_dust_box);
 
         let ignore = read_ignore_file();
@@ -32,39 +32,45 @@ impl Command for Sweep {
             .difference(&ignore)
             .cloned()
             .collect::<Vec<String>>();
+        move_files_to_dust_box(target_files, &path_to_dust_box);
 
-        for target in target_files.iter() {
-            let target_path = path_buf![&target];
-            let target_name = extract_file_name(&target_path);
-            let target_base = extract_base(&target_path);
-            let to          = path_buf![&path_to_dust_box, target_base];
+        move_empty_dir_to_dust_box(&path_to_dust_box);
+    }
+}
 
-            create_essential_dir_all(&to);
+fn move_files_to_dust_box(target_files: Vec<String>, path_to_dust_box: &PathBuf) {
+    for target in target_files.iter() {
+        let target_path = path_buf![&target];
+        let target_name = extract_file_name(&target_path);
+        let target_base = extract_base(&target_path);
+        let to          = path_buf![&path_to_dust_box, target_base];
 
-            println!(r#"EXECUTION: Move "{}" to dust box."#, target_name);
+        create_essential_dir_all(&to);
 
-            // forcedly overwrite if the file exists with same name.
-            match fs::rename(target, path_buf![to, target_name]) {
-                Ok(_)    => println!("  OK: Moved the file to dust box."),
-                Err(why) => panic!("  ERROR: {}", why),
-            }
+        println!(r#"EXECUTION: Move "{}" to dust box."#, target_name);
+
+        // forcedly overwrite if the file exists with same name.
+        match fs::rename(target, path_buf![to, target_name]) {
+            Ok(_)    => println!("  OK: Moved the file to dust box."),
+            Err(why) => panic!("  ERROR: {}", why),
         }
+    }
+}
 
-        // delete all empty directories.
-        let all_dirs = enumerate_only_dirs_under(".");
-        for dir in all_dirs.iter().filter(|d| *d != ".") {
-            println!(r#"EXECUTION: Remove "{}" directory."#, dir);
+fn move_empty_dir_to_dust_box(path_to_dust_box: &PathBuf) {
+    let all_dirs = enumerate_only_dirs_under(".");
+    for dir in all_dirs.iter().filter(|d| *d != ".") {
+        println!(r#"EXECUTION: Remove "{}" directory."#, dir);
 
-            match fs::remove_dir(dir) {
-                Ok(_) => {
-                    println!("  OK: Removed the directory.");
-                    create_essential_dir_all(&path_buf![&path_to_dust_box, dir]);
-                },
-                Err(why) => match why.raw_os_error() {
-                    Some(39) => println!("  NOTICE: the directory is not empty."),
-                    _        => panic!("  ERROR: {}", why),
-                },
-            }
+        match fs::remove_dir(dir) {
+            Ok(_) => {
+                println!("  OK: Removed the directory.");
+                create_essential_dir_all(&path_buf![&path_to_dust_box, dir]);
+            },
+            Err(why) => match why.raw_os_error() {
+                Some(39) => println!("  NOTICE: the directory is not empty."),
+                _        => panic!("  ERROR: {}", why),
+            },
         }
     }
 }
