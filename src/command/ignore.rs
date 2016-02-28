0860f682e50c02c376844768306e2350aae45095
ttokutake <tadatoshi.tokutake@gmail.com>
2016-02-28 20:21:21 +0900

    [Feature] Create "ignore add" command

--- src/command/ignore.rs
+++ src/command/ignore.rs
@@ -1,5 +1,13 @@
 use super::Command;
 
+use constant::*;
+use lib::io::*;
+use lib::setting::*;
+use std::collections::BTreeSet;
+use std::fs::File;
+use std::io::Write;
+use std::path::{MAIN_SEPARATOR, Path};
+
 pub struct Ignore {
     pub command: Option<String>,
     pub paths  : Vec<String>,
@@ -27,12 +35,53 @@ impl Command for Ignore {
 }
 
 
+fn append_prefix_if_need(path: &String) -> String {
+    let prefix       = format!(".{}", MAIN_SEPARATOR);
+    let prefix: &str = prefix.as_ref();
+    format!("{}{}", if path.starts_with(prefix) { "" } else { prefix }, path)
+}
+
 fn add(ignore: &Ignore) {
     let paths = &ignore.paths;
+
     if paths.len() == 0 {
         return ignore.help();
     }
-    println!("do add command with {:?}", paths);
+
+    print_with_tag(0, Tag::Execution, format!("Confirm files to be added to \"{}\"", IGNORE_FILE_NAME));
+
+    let ignores_to_be_added = paths
+        .iter()
+        .map(append_prefix_if_need)
+        .filter(|p| Path::new(p).is_file())
+        .collect::<BTreeSet<String>>();
+
+    if ignores_to_be_added.len() == 0 {
+        return print_with_tag(1, Tag::Warning, "The files do not exist to be added");
+    }
+
+    for file in &ignores_to_be_added {
+        print_with_tag(1, Tag::Okay, format!("\"{}\" will be ignored", file));
+    }
+
+    let new_ignores = read_ignore_file()
+        .union(&ignores_to_be_added)
+        .fold(String::new(), |c, ref f| c + f + "\n");
+
+    print_with_tag(0, Tag::Execution, format!("Recreate \"{}\" file", IGNORE_FILE_NAME));
+
+    match File::create(ignore_file()) {
+        Ok(mut f) => {
+            print_with_tag(1, Tag::Okay, "Created the file");
+
+            print_with_tag(0, Tag::Execution, "Write the file contents");
+            match f.write(new_ignores.as_bytes()) {
+                Ok(_)    => print_with_tag(1, Tag::Okay, "Done"),
+                Err(why) => print_with_tag(1, Tag::Error, why),
+            }
+        },
+        Err(why) => print_with_tag(1, Tag::Error, why),
+    }
 }
 
 fn remove(ignore: &Ignore) {
