31262cc338b480e5426376faffb7243f65369ca1
ttokutake <tadatoshi.tokutake@gmail.com>
2016-04-17 16:48:13 +0900

    [Feature] Create Storage.print_and_log()

--- src/lib/setting/storage.rs
+++ src/lib/setting/storage.rs
@@ -62,14 +62,14 @@ impl Storage {
         path_buf![self.path_to_box(), &file_name]
     }
 
-    fn write_log<U: AsRef<[u8]>>(&self, content: U) -> Result<usize, IoError> {
+    fn write_log<S: AsRef<str>>(&self, content: S) -> Result<usize, IoError> {
         let file = OpenOptions::new()
             .create(true)
             .append(true)
             .open(self.path_to_log());
         let mut file = try!(file);
 
-        file.write(content.as_ref())
+        file.write(format!("{}\n", content.as_ref()).as_bytes())
     }
 
     fn start_mark_for_log(&self) -> String {
@@ -77,7 +77,7 @@ impl Storage {
         format!(
             "############
 # {} #
-############\n",
+############",
             datetime,
         )
     }
@@ -103,7 +103,7 @@ impl Storage {
     pub fn squeeze_dusts<P: AsRef<Path>>(&self, paths_to_dust: Vec<P>) -> Result<(), IoError> {
         let path_to_dust_box = self.path_to_dust_box();
 
-        print_with_tag(Tag::Info, format!("Move dusts to \"{}\"", path_to_dust_box.display()));
+        self.print_and_log(format!("Move dusts to \"{}\"", path_to_dust_box.display()));
 
         for path_to_dust in &paths_to_dust {
             let path_to_dust = path_to_dust.as_ref();
@@ -118,8 +118,7 @@ impl Storage {
 
             let to = path_buf![&path_to_dust_box, target_base];
 
-            let message = format!("  => \"{}\"", path_to_dust.display());
-            print_with_tag(Tag::Info, message);
+            self.print_and_log(format!("  => \"{}\"", path_to_dust.display()));
 
             try!(fs::create_dir_all(&to));
 
@@ -127,7 +126,7 @@ impl Storage {
             match fs::rename(path_to_dust, path_buf![to, target_file]) {
                 Ok(_)  => (),
                 Err(e) => match e.kind() {
-                    IoErrorKind::PermissionDenied => print_with_tag(Tag::Info, "     Interrupted for permission"),
+                    IoErrorKind::PermissionDenied => self.print_and_log("     Interrupted for permission"),
                     _                             => return Err(From::from(e)),
                 },
             };
@@ -139,19 +138,18 @@ impl Storage {
     pub fn squeeze_empty_dirs_only<P: AsRef<Path>>(&self, paths_to_dir: Vec<P>) -> Result<(), IoError> {
         let path_to_dust_box = self.path_to_dust_box();
 
-        print_with_tag(Tag::Info, format!("Move empty dirs to \"{}\"", path_to_dust_box.display()));
+        self.print_and_log(format!("Move empty dirs to \"{}\"", path_to_dust_box.display()));
 
         for path_to_dir in &paths_to_dir {
             if is_empty_dir(path_to_dir) {
                 let path_to_dir = path_to_dir.as_ref();
 
-                let message = format!("  => \"{}\"", path_to_dir.display());
-                print_with_tag(Tag::Info, message);
+                self.print_and_log(format!("  => \"{}\"", path_to_dir.display()));
 
                 match fs::remove_dir(path_to_dir) {
                     Ok(_)  => try!(fs::create_dir_all(path_buf![&path_to_dust_box, path_to_dir])),
                     Err(e) => match e.kind() {
-                        IoErrorKind::PermissionDenied => print_with_tag(Tag::Info, "     Interrupted for permission"),
+                        IoErrorKind::PermissionDenied => self.print_and_log("     Interrupted for permission"),
                         _                             => return Err(From::from(e)),
                     },
                 };
@@ -165,7 +163,7 @@ impl Storage {
     pub fn delete_expired_boxes(&self, moratorium: Duration) -> Result<(), IoError> {
         let path_to_storage = Self::path();
 
-        print_with_tag(Tag::Info, "Delete expired dusts");
+        self.print_and_log("Delete expired dusts");
 
         let target_boxes = try!(ls(&path_to_storage))
             .into_iter()
@@ -177,10 +175,19 @@ impl Storage {
             .collect::<Vec<PathBuf>>();
 
         for target_box in &target_boxes {
-            print_with_tag(Tag::Info, format!("  => \"{}\"", target_box.display()));
+            self.print_and_log(format!("  => \"{}\"", target_box.display()));
             try!(fs::remove_dir_all(target_box));
         };
 
         Ok(())
+    }
+
+
+    fn print_and_log<S: AsRef<str>>(&self, message: S) {
+        let message = message.as_ref();
+        print_with_tag(Tag::Info, message);
+        if self.log_file.is_some() {
+            self.write_log(message).ok();
+        }
     }
 }
