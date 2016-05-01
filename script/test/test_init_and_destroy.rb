7f20fdf4fe42f441d8ec6f78e45360ac3411652e
ttokutake <tadatoshi.tokutake@gmail.com>
2016-05-01 20:02:23 +0900

    [Test] Add test for "init" command

--- script/test/test_init_and_destroy.rb
+++ script/test/test_init_and_destroy.rb
@@ -1,5 +1,7 @@
 require 'test/unit'
 
+BASE_DIR = '.kic'
+
 def build!
   `cargo build`
   raise 'Build failed.' if $? != 0
@@ -13,19 +15,21 @@ def initialize_kic!
 end
 
 def destroy_kic!
-  `
-    expect -c '
-      set timeout 5
-      spawn cargo run destroy
-      expect {
-        default { exit 1 }
-        -regexp "\\\\\\[yes/no\\\\\\]:\\\\s*$"
-      }
-      send "yes\\n"
-      expect eof
-    '
-  `
-  raise 'Failed to destroy.' if $? != 0
+  if Dir.exists?('.kic') then
+    `
+      expect -c '
+        set timeout 5
+        spawn cargo run destroy
+        expect {
+          default { exit 1 }
+          -regexp "\\\\\\[yes/no\\\\\\]:\\\\s*$"
+        }
+        send "yes\\n"
+        expect eof
+      '
+    `
+    raise 'Failed to destroy.' if $? != 0
+  end
 end
 
 class TestInitAndDestroy < Test::Unit::TestCase
@@ -35,20 +39,35 @@ class TestInitAndDestroy < Test::Unit::TestCase
     end
   end
 
-  def setup
-    @base_dir = '.kic'
+  def teardown
+    destroy_kic!
   end
 
+  @@storage  = "#{BASE_DIR}/warehouse"
+  @@config   = "#{BASE_DIR}/config.toml"
+  @@ignore   = "#{BASE_DIR}/ignore"
+
+
   def test_basic_init_and_destroy
-    assert !Dir.exists?(@base_dir)
+    assert !Dir.exists?(BASE_DIR)
 
     initialize_kic!
-    assert Dir .exists?(@base_dir)
-    assert Dir .exists?("#{@base_dir}/warehouse")
-    assert File.exists?("#{@base_dir}/config.toml")
-    assert File.exists?("#{@base_dir}/ignore")
+    assert Dir .exists?(BASE_DIR)
+    assert Dir .exists?(@@storage)
+    assert File.exists?(@@config)
+    assert File.exists?(@@ignore)
 
     destroy_kic!
-    assert !Dir.exists?(@base_dir)
+    assert !Dir.exists?(BASE_DIR)
+  end
+
+  def test_init_should_not_delete_existing_config_and_ignore
+    initialize_kic!
+
+    File.open(@@config, "w").close
+    File.open(@@ignore, "w").close
+    initialize_kic!
+    assert File.zero?(@@config)
+    assert File.zero?(@@ignore)
   end
 end
